2e44a4a5ac58c8ec778ba8b0806217de7c28556f
Justin Fiedler <justin@iterative.ly>
2020-07-02 10:11:22 -0700

    Code clean up and removed a bunch of unecessary stuff

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,9 @@ import * as serviceWorker from './serviceWorker'
 import App, { LocalStorageKey } from './App'
 import ErrorBoundary from './ErrorBoundary'
 import { NotFound } from './NotFound'
-import itly from '@itly/sdk';
+import itly from '@itly/sdk'
 
-itly.load({});
+itly.load({})
 
 export type Routes = '/' | '/active' | '/completed'
 
